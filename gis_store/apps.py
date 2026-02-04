a78acbb978b421a13d275befbeddd320db42e452
Thao Tran <tranthithanhthao625@gmail.com>
2026-02-04 21:57:30 +0700

    thay doi mot vai chu trong

--- gis_store/apps.py
+++ gis_store/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class GisStoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gis_store'
+    verbose_name = "Hệ thống quản lý"
